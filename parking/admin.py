74fa7236d70521e292af9133b357e6d34a1f64d5
patelkavan <patelkavan27@gmail.com>
2017-04-12 15:52:43 -0700

    Added fields to admin.py

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Spots, Parked
 
 # Register your models here.
+class SpotsAdmin(admin.ModelAdmin):
+    list_display = ["id", "lng", "lat", "occupied"]
+    class Meta:
+        model = Spots
+admin.site.register(Spots, SpotsAdmin)
+
+class ParkedAdmin(admin.ModelAdmin):
+    list_display = ["id", "parking_spot", "time_range"]
+    class Meta:
+        model = Parked
+admin.site.register(Parked, ParkedAdmin)
