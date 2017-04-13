d92da81bf6972e68bf63752af7edc3bf66e11251
patelkavan <patelkavan27@gmail.com>
2017-04-12 19:46:03 -0700

    Added fields to admin.py

--- parking/models.py
+++ parking/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Spots(models.Model):
-	lng = models.IntegerField(blank=False, default='')
-	lat = models.IntegerField(blank=False, default='')
+	lng = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
+	lat = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
 	radius = models.CharField(max_length=100)
 	occupied = models.BooleanField(default=False)
 
