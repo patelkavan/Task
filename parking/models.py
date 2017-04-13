21b5074eee18782f060d69349d0751dfb4822196
patelkavan <patelkavan27@gmail.com>
2017-04-12 22:24:06 -0700

    Task#2

--- parking/models.py
+++ parking/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Spots(models.Model):
 	lng = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
 	lat = models.DecimalField(blank=False, max_digits=5, decimal_places=2)
-	radius = models.CharField(max_length=100)
 	occupied = models.BooleanField(default=False)
 
 class Parked(models.Model):
